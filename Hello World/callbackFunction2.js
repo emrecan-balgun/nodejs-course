878b0087a0ee5baa70752818304c674d8f0be33e
Emrecan Balgün <deathlygrave@gmail.com>
2022-12-12 19:21:27 +0300

    add callback function example 2

---
+++ Hello World/callbackFunction2.js
@@ -0,0 +1,21 @@
+function birinci(callback) {
+   setTimeout(function () {
+      console.log("birinci fonksiyon");
+      callback(ucuncu);
+   }, 1000);
+
+}
+
+function ikinci(callback) {
+   setTimeout(function () {
+      console.log("ikinci fonksiyon");
+      callback();
+   }, 2000);
+}
+
+function ucuncu() {
+   console.log("ucuncu fonksiyon");
+}
+
+birinci(ikinci);
+
