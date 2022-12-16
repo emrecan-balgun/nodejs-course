38daec3b80ba2d686ea35fe610d4083e47b2f613
Emrecan Balgün <deathlygrave@gmail.com>
2022-12-16 15:10:51 +0300

    add write file to fs module

--- FS Module/index.js
+++ FS Module/index.js
@@ -1,11 +1,28 @@
 const fs = require('fs');
 
 // Read file
-fs.readFile('password.txt', 'utf8', (error, data) => {
-    if(error) {
-        console.log("Error: ", error);
-    } else {
-        console.log("File read successfully!\nData: ", data);
-    }
-});
+// fs.readFile('password.txt', 'utf8', (error, data) => {
+//     if(error) {
+//         console.log("Error: ", error);
+//     } else {
+//         console.log("File read successfully!\nData: ", data);
+//     }
+// });
 
+// Write file
+// fs.writeFile('username.txt', 'test', 'utf8', (error) => {
+//     if(error) {
+//         console.log("Error: ", error);
+//     } else {
+//         console.log("File write successfully!");
+//     }
+// })
+
+// Write file (JSON)
+// fs.writeFile('test.json', '{"name": "Emrecan Balgün", "age": 24}', 'utf8', (error) => {
+//     if(error) {
+//         console.log("Error: ", error);
+//     } else {
+//         console.log("File write successfully!");
+//     }
+// })
