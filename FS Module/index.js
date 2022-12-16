6339873c7bd756e698a7ca444695717a54a94d6e
Emrecan Balgün <deathlygrave@gmail.com>
2022-12-16 15:21:19 +0300

    add some functions to fs module

--- FS Module/index.js
+++ FS Module/index.js
@@ -16,7 +16,7 @@ const fs = require('fs');
 //     } else {
 //         console.log("File write successfully!");
 //     }
-// })
+// });
 
 // Write file (JSON)
 // fs.writeFile('test.json', '{"name": "Emrecan Balgün", "age": 24}', 'utf8', (error) => {
@@ -25,4 +25,43 @@ const fs = require('fs');
 //     } else {
 //         console.log("File write successfully!");
 //     }
-// })
+// });
+
+// Append file
+// fs.appendFile('username.txt', '\nadded line', 'utf8', (error) => {
+//     if(error) {
+//         console.log("Error: ", error);
+//     } else {
+//         console.log("File append successfully!");
+//     }
+// });
+
+// Delete file
+// fs.unlink('test.json', (error) => {
+//     if(error) {
+//         console.log("Error: ", error);
+//     } else {
+//         console.log("File deleted successfully!");
+//     }
+// });
+
+// Make directory
+// Recursive: true needs to be used to create nested directories
+// fs.mkdir('uploads/img', { recursive: true }, error => {
+//     if(error) {
+//         console.log("Error: ", error);
+//     } else {
+//         console.log("Directory created successfully!");
+//     }
+// });
+
+// Delete directory
+// Recursive: true needs to be used to delete nested directories
+// fs.rmdir to fs.rm
+// fs.rm('uploads', { recursive: true }, error => {
+//     if(error) {
+//         console.log("Error: ", error);
+//     } else {
+//         console.log("Directory deleted successfully!");
+//     }
+// });
