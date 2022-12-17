0b62c6e74ec7537cda54b5ab7ba134e3d0ee699c
Emrecan Balgün <deathlygrave@gmail.com>
2022-12-17 16:20:51 +0300

    add url route to first server

--- First Server/index.js
+++ First Server/index.js
@@ -2,8 +2,21 @@ const http = require('http');
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-    console.log("Request received");
-    res.write('Hello World');
+    // console.log("Request received");
+    const url = req.url;
+    if(url === '/') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write("<h1>Index Page</h1>");
+    } else if(url === '/about') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write("About Page");
+    } else if(url === '/contact'){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write("Contact Page");
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.write("<h1>404 Page</h1>");
+    }
     res.end();
 });
 
