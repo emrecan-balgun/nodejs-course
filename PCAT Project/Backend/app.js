c4fc6c99e8e3093b2305d6397d9aa8bf4fed9dd5
Emrecan Balgün <deathlygrave@gmail.com>
2022-12-19 19:36:35 +0300

    add backend files

--- PCAT Project/Backend/app.js
+++ PCAT Project/Backend/app.js
@@ -1,5 +1,17 @@
-const fn = function () {
-  console.log('Hello World');
-};
+const express = require('express');
+const app = express();
+const PORT = 3000;
 
-fn();
+app.get('/', (req, res) => {
+  const photo = {
+    id: 1,
+    name: 'Photo 1',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, ipsum!',
+  };
+  res.send(photo);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
