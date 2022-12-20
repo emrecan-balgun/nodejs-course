d3d96b2146618f9ae895765d0879ae9988f21f82
Emrecan Balgün <deathlygrave@gmail.com>
2022-12-20 19:12:09 +0300

    add ejs and update app.js

--- PCAT Project/app.js
+++ PCAT Project/app.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
+
 const app = express();
 const PORT = 3000;
+
+// TEMPLATE ENGINE
+app.set('view engine', 'ejs');
 
 // MIDDLEWARES
 app.use(express.static('public'));
 
+// ROUTES
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './public/index.html'));
+  // res.sendFile(path.resolve(__dirname, './public/index.html'));
+  res.render('index');
 });
 
 app.listen(PORT, () => {
