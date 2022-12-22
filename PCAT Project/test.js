05d4e6d059be9c9374e2e5d8313c3e8b89267f03
Emrecan Balgün <deathlygrave@gmail.com>
2022-12-22 15:32:50 +0300

    add crud to pcat project

--- PCAT Project/test.js
+++ PCAT Project/test.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+// set strictQuery to false (for warning)
+mongoose.set('strictQuery', false);
+
 // connect DB
-mongoose.connect('mongodb://localhost/pcat-test-db');
+mongoose.connect('mongodb://localhost/pcat-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // create a schema
 const PhotoSchema = new Schema({
@@ -14,7 +20,33 @@ const PhotoSchema = new Schema({
 const Photo = mongoose.model('Photo', PhotoSchema);
 
 // create a photo
-Photo.create({
-  title: 'Photo Title 1',
-  description: 'Photo Description 1 lorem ipsum dot color',
-});
+// Photo.create({
+//   title: 'Photo Title 3',
+//   description: 'Photo Description 3 lorem ipsum dot color',
+// });
+
+// read a photo
+// Photo.find({}, (err, data) => {
+//   console.log(data);
+// });
+
+// update a photo
+// const id = '63a44d1d140ea7460e907c77';
+// Photo.findByIdAndUpdate(
+//   id,
+//   {
+//     title: 'Photo Title 3 Updated 2',
+//     description: 'Photo Description 3 Updated 2',
+//   },
+//   // return updated data
+//   { new: true },
+//   (err, data) => {
+//     console.log(data);
+//   }
+// );
+
+// delete a photo
+// const id = '63a44d1d140ea7460e907c77';
+// Photo.findByIdAndDelete(id, (err, data) => {
+//   console.log(data);
+// });
