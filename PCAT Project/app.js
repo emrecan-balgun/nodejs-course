3e386cdbd4f1adf8a7bf19e23c6cf11bac479161
Emrecan Balgün <deathlygrave@gmail.com>
2022-12-23 20:58:08 +0300

    add update photo detail on pcat project

--- PCAT Project/app.js
+++ PCAT Project/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override');
 const ejs = require('ejs');
 const path = require('path');
 const fs = require('fs');
@@ -26,6 +27,7 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(fileUpload());
+app.use(methodOverride('_method'));
 
 // ROUTES
 app.get('/', async (req, res) => {
@@ -67,6 +69,22 @@ app.post('/photos', async (req, res) => {
     });
     res.redirect('/');
   });
+});
+
+app.get('/photos/edit/:id', async (req, res) => {
+  const photo = await Photo.findById(req.params.id);
+  res.render('edit', {
+    photo,
+  });
+});
+
+app.put('/photos/:id', async (req, res) => {
+  let photo = await Photo.findById(req.params.id);
+  photo.title = req.body.title;
+  photo.description = req.body.description;
+  await photo.save();
+
+  res.redirect(`/photos/${req.params.id}`);
 });
 
 app.listen(PORT, () => {
