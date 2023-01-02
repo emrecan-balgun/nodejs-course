deeff5fae0a3d0e54ee6329500c6f44c2054babe
Emrecan Balgün <deathlygrave@gmail.com>
2023-01-02 18:52:35 +0300

    add mongoose connection and controllers, routes

--- SmartEDU Project/app.js
+++ SmartEDU Project/app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+const pageRoute = require('./routes/pageRoute');
 
 const app = express();
 dotenv.config();
+
+// Mongoose Config
+mongoose.set('strictQuery', false);
+
+// Connect to MongoDB
+mongoose.connect(`mongodb+srv://admin:${process.env.MONGODB_PASSWORD}@cluster0.abdismo.mongodb.net/smartedu`)
+  .then(() => console.log('Connected DB!'));
 
 // Template Engine
 app.set('view engine', 'ejs');
@@ -11,17 +21,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 // Routes
-app.get('/', (req, res) => {
-  res.status(200).render('index', {
-    page_name: 'index',
-  });
-});
-
-app.get('/about', (req, res) => {
-  res.status(200).render('about', {
-    page_name: 'about',
-  });
-});
+app.use('/', pageRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
