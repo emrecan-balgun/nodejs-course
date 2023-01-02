082e88707c0b748837553b05122d99138a6a1148
Emrecan Balgün <deathlygrave@gmail.com>
2023-01-02 22:33:21 +0300

    add get course by id and update course page

--- SmartEDU Project/controllers/courseController.js
+++ SmartEDU Project/controllers/courseController.js
@@ -21,15 +21,30 @@ exports.getAllCourse = async (req, res) => {
   try {
     const courses = await Course.find().sort({ createdAt: -1 });
     res.status(200).render('courses', {
-        page_name: 'courses',
-        courses,
-    })
+      page_name: 'courses',
+      courses,
+    });
     // res.status(200).json({
     //   status: 'success',
     //   data: {
     //     courses,
     //   },
     // });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: error,
+    });
+  }
+};
+
+exports.getCourse = async (req, res) => {
+  try {
+    const course = await Course.findById(req.params.id);
+    res.status(200).render('course', {
+      page_name: 'courses',
+      course,
+    });
   } catch (error) {
     res.status(400).json({
       status: 'fail',
--- SmartEDU Project/routes/courseRoute.js
+++ SmartEDU Project/routes/courseRoute.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.route('/').post(courseController.createCourse); // localhost:3000/courses
 router.route('/').get(courseController.getAllCourse); // localhost:3000/courses
+router.route('/:id').get(courseController.getCourse); // localhost:3000/courses/:id
 
 module.exports = router;
