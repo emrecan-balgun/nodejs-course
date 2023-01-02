65031f711f059ce10940fb75300502b1996ea7fa
Emrecan Balgün <deathlygrave@gmail.com>
2023-01-02 22:12:04 +0300

    add course and update courses page

--- SmartEDU Project/app.js
+++ SmartEDU Project/app.js
@@ -3,12 +3,13 @@ const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
 const pageRoute = require('./routes/pageRoute');
+const courseRoute = require('./routes/courseRoute');
 
 const app = express();
 dotenv.config();
 
 // Mongoose Config
-mongoose.set('strictQuery', false);
+mongoose.set('strictQuery', false); 
 
 // Connect to MongoDB
 mongoose.connect(`mongodb+srv://admin:${process.env.MONGODB_PASSWORD}@cluster0.abdismo.mongodb.net/smartedu`)
@@ -19,9 +20,12 @@ app.set('view engine', 'ejs');
 
 // Middlewares
 app.use(express.static('public'));
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 // Routes
 app.use('/', pageRoute);
+app.use('/courses', courseRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
---
+++ SmartEDU Project/controllers/courseController.js
@@ -0,0 +1,39 @@
+const Course = require('../models/Course');
+
+exports.createCourse = async (req, res) => {
+  try {
+    const course = await Course.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        course,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: error,
+    });
+  }
+};
+
+exports.getAllCourse = async (req, res) => {
+  try {
+    const courses = await Course.find().sort({ createdAt: -1 });
+    res.status(200).render('courses', {
+        page_name: 'courses',
+        courses,
+    })
+    // res.status(200).json({
+    //   status: 'success',
+    //   data: {
+    //     courses,
+    //   },
+    // });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: error,
+    });
+  }
+};
