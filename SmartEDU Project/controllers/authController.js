83d9c2fc9962386fa3aa6bd8760650f87ad8106d
Emrecan Balgün <deathlygrave@gmail.com>
2023-01-03 15:50:46 +0300

    add login

--- SmartEDU Project/controllers/authController.js
+++ SmartEDU Project/controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 exports.signup = async (req, res) => {
   try {
@@ -13,6 +14,28 @@ exports.signup = async (req, res) => {
     res.status(400).json({
       status: 'fail',
       message: error,
+    });
+  }
+};
+
+exports.signin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (user) {
+      bcrypt.compare(password, user.password, (err, same) => {
+        if (same) {
+          // user session
+          res.status(200).send('Logged in');
+        } else {
+          res.status(401).send('Incorrect password or email');
+        }
+      });
+    }
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
     });
   }
 };
--- SmartEDU Project/routes/userRoute.js
+++ SmartEDU Project/routes/userRoute.js
@@ -5,5 +5,6 @@ const authController = require('../controllers/authController');
 const router = express.Router();
 
 router.route('/signup').post(authController.signup); // localhost:3000/users/signup
+router.route('/signin').post(authController.signin); // localhost:3000/users/signin
 
 module.exports = router;
